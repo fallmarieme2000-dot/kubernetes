f027a37db5c0786e2bf16dd6f58752a06788629b
marieme <fallmarieme2000@gmail.com>
2025-10-16 13:07:29 +0000

    test-kub

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './styles/App.css'
 
-import Classe from './components/Classe.jsx'
-
-createRoot(document.getElementById('root')).render(
-    
-    <Classe />
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
