c5459a661992ccd1afe93e6b35f5b9738cfc2e8d
fallmarieme2000-dot <fallmarieme2000@gmail.com>
2025-10-17 12:31:39 +0000

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,9 @@ app.use(express.json({ limit: "10mb" }));
 
 app.use(express.json());
 
-app.use(cors()); 
+app.use(cors({
+  origin: "http://localhost:30419"
+}));
 
 // Routes API
 app.use('/api', smartphoneRoutes);
